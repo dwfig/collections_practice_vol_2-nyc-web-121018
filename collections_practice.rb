a90b2d098f954c88a00eb900bb5250685d41e56d
Daniel Fries <daniel.w.fries@gmail.com>
2018-12-07 00:35:18 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,36 @@
 # your code goes here
+def begins_with_r(array)
+##
+array.all? {|e| e.start_with?("r")}
+end
+
+def contain_a(array)
+##
+array.find_all {|e| e.include?("a")}
+end
+
+def first_wa(array)
+##
+array.find {|e| e.to_s.start_with?("wa")}
+end
+
+def remove_non_strings(array)
+##
+array.select {|e| e.class == String}
+end
+
+def count_elements(array, element)
+##
+end
+
+def merge_data(struct1, struct2)
+##
+end
+
+def find_cool(hashlist)
+##
+end
+
+def organize_schools(schoollist)
+##
+end
